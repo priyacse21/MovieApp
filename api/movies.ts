a78d59a11f581ed1964dd36263fd71feee2e638f
Priya <priyakaruppsamy2003@gmail.com>
2025-09-11 12:55:40 +0530

    BasicFetchApi

--- api/movies.ts
+++ api/movies.ts
@@ -1,4 +1,4 @@
-export const fetchTopRatedMoies = ()=>{
+export const fetchTopRatedMoies = async ()=>{
 const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
 const options = {
   method: 'GET',
@@ -8,8 +8,25 @@ const options = {
   }
 };
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
+
+
+const res=await fetch(url, options);
+
+if(!res.ok)
+{
+  throw new Error('Failed to fetch movies');
+}
+const json=await res.json();
+return json.results
+
+
+
+
+
+
+
+
+
+
+
 };
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,50 @@
-import { StyleSheet } from 'react-native';
+import {ActivityIndicator, FlatList, StyleSheet,Text } from 'react-native';
 import { View } from '@/components/Themed';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchTopRatedMoies } from '@/api/movies';
-
-export default function TabOneScreen() {
   
+export default function TabOneScreen() {
+  const [movies,setMovies] = useState([]);
+  const [isLoading, setIsLoading]=useState(false);
+  const [error,setError]=useState(null);
+
   useEffect(()=>{
-    fetchTopRatedMoies();
+    const fetchMovies = async () =>{
+      setIsLoading(true);
+      try{
+          const movies=await fetchTopRatedMoies();
+           setMovies(movies);
+      }
+      catch(error)
+      {
+        setError(error.message)
+      }
+
+    setIsLoading(false);
+    };
+    fetchMovies();
   },[])
+
+  if(isLoading)
+  {
+    <ActivityIndicator/>
+  }
+
+  if(error)
+  {
+    return <Text>{error.message}</Text>
+  }
   return (
     <View style={styles.container}>
-
+      {/* renders list of data */}
+      <FlatList
+      data={movies}
+      renderItem={({item})=>(
+        <View>
+          <Text>{item.title}</Text>
+          </View>
+      )}
+      />
     </View>
   );
 }
